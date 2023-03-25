ff3b0febf05bdeda2eb3852463b30fae0987aea4
Mars <arsenmanarbekov@yandex.kz>
2023-03-25 20:31:15 +0600

    Complete ex2

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿///Напишите программу, которая на вход принимает
+///два числа и выдаёт, какое число большее, а какое меньшее
+///a = 5; b = 7 -> max = 7
+///a = 2 b = 10 -> max = 10
+///a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    int max = number1;
+    Console.WriteLine($"Первое число {number1} больше!");
+
+}
+else
+{
+    int max = number2;
+    Console.WriteLine($"Второе число {number2} больше!");
+}
