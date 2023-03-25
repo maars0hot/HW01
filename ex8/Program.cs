90b57c01447f6e9ec09287c6ffb7fdb3d7735e60
Mars <arsenmanarbekov@yandex.kz>
2023-03-25 21:52:58 +0600

    Complete ex8

--- ex8/Program.cs
+++ ex8/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает 
+//число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count <= number)     ///очень тяжело даются циклы , всё , что ниже очень плохо понимаю
+{
+    if (count %2 == 0)             
+    Console.Write(count +",");  
+    count++; 
+     
+}
