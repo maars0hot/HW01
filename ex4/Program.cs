f8289773e8ca6ded59c5928f112dadfd4682f6f4
Mars <arsenmanarbekov@yandex.kz>
2023-03-25 20:53:38 +0600

    Complete ex4

--- ex4/Program.cs
+++ ex4/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿///Задача 4: Напишите программу, которая принимает на вход 
+///три числа и выдаёт максимальное из этих чисел.
+///2, 3, 7 -> 7
+///44 5 78 -> 78
+///22 3 9 -> 22
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (max < num2 )
+{
+    max = num2;
+}
+else
+{
+    max = num1;
+}
+if (max > num3)
+{
+    max = num1;
+}
+else
+{
+    max = num3;
+}
+    
+Console.WriteLine($"Максимальное число:{max}");
